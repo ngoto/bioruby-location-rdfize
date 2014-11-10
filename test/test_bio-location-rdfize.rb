752beb6777403603e467d2dce4d2de7f21bff313
Naohisa Goto <ng@bioruby.org>
2014-11-10 17:12:03 +0900

    add tests

--- test/test_bio-location-rdfize.rb
+++ test/test_bio-location-rdfize.rb
@@ -1,7 +1,51 @@
+
+# To run this test,
+#  % bundle exec rake test
+# or
+#  % rake test
+#
+
 require 'helper'
+require 'bio'
 
 class TestBioLocationRdfize < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  PREFIX = "http://localhost/test#".freeze
+
+  def test_forward
+    loc = Bio::Locations.new("123..456")[0]
+
+    expected = <<EXPECTED
+<http://localhost/test#Location123-456:1> a <http://biohackathon.org/resource/faldo#Region> ;
+    <http://biohackathon.org/resource/faldo#begin> <http://localhost/test#Location123-456:1:begin> ;
+    <http://biohackathon.org/resource/faldo#end> <http://localhost/test#Location123-456:1:end> .
+
+<http://localhost/test#Location123-456:1:begin> a <http://biohackathon.org/resource/faldo#ExactPosition>, <http://biohackathon.org/resource/faldo#ForwardStrandedPosition> ;
+    <http://biohackathon.org/resource/faldo#position> 123 .
+<http://localhost/test#Location123-456:1:end> a <http://biohackathon.org/resource/faldo#ExactPosition>, <http://biohackathon.org/resource/faldo#ForwardStrandedPosition> ;
+    <http://biohackathon.org/resource/faldo#position> 456 .
+
+EXPECTED
+
+    assert_equal(expected, loc.rdfize(PREFIX))
   end
+
+  def test_reverse
+    loc = Bio::Locations.new("complement(123..456)")[0]
+
+    expected = <<EXPECTED
+<http://localhost/test#Location-:-1> a <http://biohackathon.org/resource/faldo#Region> ;
+    <http://biohackathon.org/resource/faldo#begin> <http://localhost/test#Location-:-1:begin> ;
+    <http://biohackathon.org/resource/faldo#end> <http://localhost/test#Location-:-1:end> .
+
+<http://localhost/test#Location-:-1:begin> a <http://biohackathon.org/resource/faldo#ExactPosition>, <http://biohackathon.org/resource/faldo#ReverseStrandedPosition> ;
+    <http://biohackathon.org/resource/faldo#position>  .
+<http://localhost/test#Location-:-1:end> a <http://biohackathon.org/resource/faldo#ExactPosition>, <http://biohackathon.org/resource/faldo#ReverseStrandedPosition> ;
+    <http://biohackathon.org/resource/faldo#position>  .
+
+EXPECTED
+
+    assert_equal(expected, loc.rdfize(PREFIX))
+  end
+
 end
